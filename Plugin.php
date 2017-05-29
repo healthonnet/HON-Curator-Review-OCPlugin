0d941532b17480fb7cc2c417ddc80fddde14f0b5
cedric frossard <adrion.dev@gmail.com>
2017-05-29 14:12:42 +0200

    extend dependencies' models

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,46 @@
 <?php namespace HON\HonCuratorReview;
 
 use System\Classes\PluginBase;
+use RainLab\User\Models\User as UserModel;
+use HON\HonCuratorUser\Models\Activity;
+
 
 class Plugin extends PluginBase
 {
+
+    public $require = ['RainLab.User', 'HON.HonCuratorUser'];
+
     public function registerComponents()
     {
     }
 
     public function registerSettings()
     {
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+        // Add extension's relations and attributes.
+        UserModel::extend(function($model) {
+            $model->hasMany['reviews'] = 'HON\HonCuratorReview\Models\Review'; // user_id
+            $model->hasMany['services'] = ['HON\HonCuratorReview\Models\Service', 'key' => 'owner_id']; // user_id
+
+            $fillables = $model->getFillable();
+            $model->fillable($fillables);
+        });
+
+        Activity::extend(function ($model) {
+            $model->belongsToMany['questions'] = [
+                'HON\HonCuratorReview\Models\Question',
+                'table'      => 'hon_honcuratorreview_activities_questions',
+                'key'        => 'a_id',
+                'otherKey'   => 'q_id'
+            ];
+        });
     }
 }
