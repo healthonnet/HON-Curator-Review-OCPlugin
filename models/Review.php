1665b344e612d130b1974668f85337d6560508bf
cedric frossard <adrion.dev@gmail.com>
2017-09-21 10:57:58 +0200

    up to 500 chars

--- models/Review.php
+++ models/Review.php
@@ -20,7 +20,7 @@ class Review extends Model
     public $rules = [
         'global_rate' => 'required|integer|min:0|max:5',
         'title' => 'required|min:3|max:50',
-        'global_comment' => 'required|min:3|max:255',
+        'global_comment' => 'required|min:3|max:500',
     ];
 
     /**
